6613abcf660c6c67b2b8e4343a95121b599462bd
Martan de Oliveira Bastos <martanbastos9@gmail.com>
2020-09-13 16:12:51 -0300

    atualização de diretorios

--- index.php
+++ index.php
@@ -1,8 +1,2 @@
-<?php
-
-/* TODO REVISAR CÁLCULO DE AMORTIZAÇÃO */
-/* FIXME REVISAR CÁLCULO DE AMORTIZAÇÃO */
-
-/**@Martan de Oliveira Bastos <martanbastos9@gmail.com> */
-phpinfo( );
-//martan
+// código
+// martan oliveira
